c49eefcd9620d1d6f32b370d88f931829c1b3792
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-04-17 23:24:25 +0200

    added fastapi and streamlit docker direct and github actions

---
+++ docker-apps/streamlit-inference/app_streamlit.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import requests
+import pandas as pd
+import io
+import json
+
+st.title('MLOps Engineering Project: Skin Cancer Detection')
+
+# FastAPI endpoint
+endpoint = 'http://localhost:8000/predict' # FASTAPIENDPOINT
+# FASTAPI_URL = ""
+# FASTAPI_PORT = ""
+# endpoint = 'http://{FASTAPI_URL:FASTAPI_PORT}/predict'
+test_csv = st.file_uploader('', type=['csv','xlsx'], accept_multiple_files=False)
+
+if test_csv:
+    test_df = pd.read_csv(test_csv)
+    st.subheader('View Sample of Test Set')
+    st.write(test_df.head())
+
+    test_bytes_obj = io.BytesIO()
+    test_df.to_csv(test_bytes_obj, index=False)  # write to BytesIO buffer
+    test_bytes_obj.seek(0) # Reset pointer to avoid EmptyDataError
+    files = {"file": ('test_dataset.csv', test_bytes_obj, "multipart/form-data")}
+
+    if st.button('Start Prediction'):
+        with st.spinner('Prediction in Progress. Please Wait...'):
+            output = requests.post(endpoint, files=files, timeout=8000)
+        st.success('Success! Click the Download button below to retrieve prediction results (JSON format')
+        st.download_button(
+            label='Download',
+            data=json.dumps(output.json()), # Download as JSON file object
+            file_name='automl_prediction_results.json'
+        )
