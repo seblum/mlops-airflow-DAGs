728fc0772c4e610ff426876284c5b1671f799552
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-06-02 10:18:31 +0200

    testing new kwargs env

--- cnn_skin_cancer/src/preprocessing.py
+++ cnn_skin_cancer/src/preprocessing.py
@@ -15,10 +15,10 @@ from utils import list_files_in_bucket, read_image_from_s3, timeit, upload_npy_t
 def data_preprocessing(mlflow_tracking_uri: str, mlflow_experiment_id: str, aws_bucket: str, **kwargs) -> None:
     mlflow.set_tracking_uri(mlflow_tracking_uri)
 
-    print(kwargs)
+    print(kwargs["ti"])
 
-    kwargs_1 = os.getenv("ti")
-    print(kwargs_1)
+    kwargs = os.getenv("ti")
+    print(kwargs)
     # Set paths within s3
     path_raw_data = f"s3://{aws_bucket}/data/"
     path_preprocessed = "preprocessed"
