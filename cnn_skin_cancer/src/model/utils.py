7e8a7e311440171cdb2a19660a4b8e2dd803ee0a
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-06-03 23:23:45 +0200

    fix import statements

--- cnn_skin_cancer/src/model/utils.py
+++ cnn_skin_cancer/src/model/utils.py
@@ -1,9 +1,8 @@
 from enum import Enum
 
+from basic_model import BasicNet
 from keras.models import Model
-
-from cnn_skin_cancer.src.model.basic_model import BasicNet
-from cnn_skin_cancer.src.model.resnet50_model import ResNet50
+from resnet50_model import ResNet50
 
 # https://towardsdatascience.com/tensorflow-class-inheritance-beautiful-code-59d2eb7cdfce
 # https://towardsdatascience.com/model-sub-classing-and-custom-training-loop-from-scratch-in-tensorflow-2-cc1d4f10fb4e
