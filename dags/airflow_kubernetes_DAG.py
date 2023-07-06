c87b29236cd2146758ac6910a01063ea2b58a3d8
seblum <64776588+seblum@users.noreply.github.com>
2023-07-06 15:30:37 +0200

    added sleep

--- dags/airflow_kubernetes_DAG.py
+++ dags/airflow_kubernetes_DAG.py
@@ -128,7 +128,9 @@ def cnn_skin_cancer_workflow():
             dict: A dictionary containing the paths to preprocessed data.
         """
         import os
+        import time
 
+        time.sleep(6000)
         from src.preprocessing import data_preprocessing
 
         aws_bucket = os.getenv("AWS_BUCKET")
